dd08bc6dbc1fc1066040a2f99f2a657dcaa44278
GumarDiana <lantan.mp4@gmail.com>
2019-05-10 21:04:18 +0300

    add correct move functions

--- main_my.py
+++ main_my.py
@@ -16,55 +16,56 @@ class Program(QWidget):
         self.show()
 
     def set_map(self, mx, my, value):
-        if mx > -1 and mx < 4 and my > -1 and my < 4:
+        if self.Exist_map(mx, my):
             map[mx][my] = value
 
-    def exist_map(self,mx, my):
-        if mx > -1 and mx < 4 and my > -1 and my < 4:
+    def get_map(self, mx, my):
+        if self.Exist_map(mx, my):
             return map[mx][my]
+        return -1
 
-    # ex > -1 and ex < 4 and ey > -1 and ey < 4 and map[ex + sx][ey + sy] == 0
+    def Exist_map(self, mx, my):
+        return mx >=0 and mx < 4 and my >= 0 and my< 4
+
     def Turn(self, ex, ey, sx, sy):  # движение на одну клетку
-        if map[ex][ey] > 0:
-            while ex > 0 and ex < 4 and ey > 0 and ey < 4 and map[ex + sx][ey + sy] == 0:
-                print(ex," ",ey)
-                map[ex + sx][ey + sy] = map[ex][ey]
+        if self.get_map(ex, ey) > 0:
+            while self.get_map(ex + sx, ey + sy) == 0:
+
+                map[ex + sx][ey + sy] = self.get_map(ex, ey)
                 map[ex][ey] = 0
-                # self.set_map(ex + sx, ey + sy, map[ex][ey])
-                # map[ex][ey] = 0
 
-                ex -= sx
-                ey -= sy
+                ex += sx
+                ey += sy
 
+            self.random_new_numbers()
 
     def kliked(self, k):
         global map
         if k == -1:
             print('left')
             for ey in range(4):
-                for ex in range(1, 4):
+                for ex in range(-1, 4):
                     self.Turn(ex, ey, -1, 0)
-            self.random_new_numbers()
+
+        elif k == 2:
+            print('up')
+            for ex in range(4):
+                for ey in range(-1, 4):
+                    self.Turn(ex, ey, 0, -1)
 
         elif k == 1:
             print('right')
             for ey in range(4):
                 for ex in range(2, -1, -1):
                     self.Turn(ex, ey, 1, 0)
-            self.random_new_numbers()
-
-        elif k == 2:
-            print('up')
-            for ex in range(4):
-                for ey in range(1, 4):
-                    self.Turn(ex, ey, 0, -1)
-            self.random_new_numbers()
 
         elif k == 0:
             print('down')
             for ex in range(4):
                 for ey in range(2, -1, -1):
                     self.Turn(ex, ey, 0, 1)
+
+        elif k == 3:
             self.random_new_numbers()
 
         self.update()
@@ -78,6 +79,9 @@ class Program(QWidget):
             self.kliked(0)
         elif e.key() == Qt.Key_Up:
             self.kliked(2)
+        elif e.key() == Qt.Key_Plus:
+            self.kliked(3)
+
 
     def paintEvent(self, event):
         qp = QPainter()
@@ -114,4 +118,5 @@ map = [[0 for i in range(4)] for ii in range(4)]
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Program()
+
     sys.exit(app.exec_())
