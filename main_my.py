cf4486c088afd8e7048d6b77c9d06634cd86375b
GumarDiana <lantan.mp4@gmail.com>
2019-05-12 19:22:18 +0300

    add new cells rule

--- main_my.py
+++ main_my.py
@@ -4,115 +4,157 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+
 class Program(QWidget):
     def __init__(self, parent=None):
         super(Program, self).__init__(parent)
         self.setStyleSheet("background-color: white")
 
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
+    def init_ui(self):
         self.setGeometry(800, 150, 350, 500)
         self.show()
 
     def set_map(self, mx, my, value):
-        if self.Exist_map(mx, my):
-            map[mx][my] = value
+        if self.exist_map(mx, my):
+            _map[mx][my] = value
 
     def get_map(self, mx, my):
-        if self.Exist_map(mx, my):
-            return map[mx][my]
+        if self.exist_map(mx, my):
+            return _map[mx][my]
         return -1
 
-    def Exist_map(self, mx, my):
-        return mx >=0 and mx < 4 and my >= 0 and my< 4
+    @staticmethod
+    def exist_map(mx, my):
+        return 0 <= mx < 4 and 0 <= my < 4
 
-    def Turn(self, ex, ey, sx, sy):  # движение на одну клетку
+    def turn(self, ex, ey, sx, sy):  # движение на одну клетку
+
         if self.get_map(ex, ey) > 0:
             while self.get_map(ex + sx, ey + sy) == 0:
 
-                map[ex + sx][ey + sy] = self.get_map(ex, ey)
-                map[ex][ey] = 0
+                _map[ex + sx][ey + sy] = self.get_map(ex, ey)
+                _map[ex][ey] = 0
 
                 ex += sx
                 ey += sy
+                self.mooved = 1
 
-            self.random_new_numbers()
+    def join(self, ex, ey, sx, sy):
 
-    def kliked(self, k):
-        global map
+        if self.get_map(ex, ey) > 0:
+            if self.get_map(ex + sx, ey + sy) == self.get_map(ex, ey):
+                self.set_map(ex + sx, ey + sy, self.get_map(ex, ey) << 1)  # сдвигаем на битовую еденицу влево
+                while self.get_map(ex - sx, ey - sy) > 0:
+                    # устанавливаем значение предыдущего в текущий
+                    self.set_map(ex, ey, self.get_map(ex - sx, ey - sy))
+                    ex -= sx
+                    ey -= sy
+                self.set_map(ex, ey, 0)
+                self.mooved = 1
+
+    global mooved
+
+    def clicked(self, k):
+
+        global _map
         if k == -1:
-            print('left')
+            sys.stdout.write('\rleft')
+            self.mooved = 0
             for ey in range(4):
                 for ex in range(-1, 4):
-                    self.Turn(ex, ey, -1, 0)
+                    self.turn(ex, ey, -1, 0)
+                for ex in range(-1, 4):
+                    self.join(ex, ey, -1, 0)
+            if self.mooved == 1:
+                self.random_new_numbers()
 
         elif k == 2:
-            print('up')
+            sys.stdout.write('\rup')
+            self.mooved = 0
             for ex in range(4):
                 for ey in range(-1, 4):
-                    self.Turn(ex, ey, 0, -1)
+                    self.turn(ex, ey, 0, -1)
+                for ey in range(-1, 4):
+                    self.join(ex, ey, 0, -1)
+            if self.mooved == 1:
+                self.random_new_numbers()
 
         elif k == 1:
-            print('right')
+            sys.stdout.write('\rright')
+            self.mooved = 0
+            # sys.stdout.write(str(bin(map[1][1])))
             for ey in range(4):
                 for ex in range(2, -1, -1):
-                    self.Turn(ex, ey, 1, 0)
+                    self.turn(ex, ey, 1, 0)
+                for ex in range(2, -1, -1):
+                    self.join(ex, ey, 1, 0)
+            if self.mooved == 1:
+                self.random_new_numbers()
 
         elif k == 0:
-            print('down')
+            sys.stdout.write('\rdown')
+            self.mooved = 0
             for ex in range(4):
                 for ey in range(2, -1, -1):
-                    self.Turn(ex, ey, 0, 1)
+                    self.turn(ex, ey, 0, 1)
+                for ey in range(2, -1, -1):
+                    self.join(ex, ey, 0, 1)
+            if self.mooved == 1:
+                self.random_new_numbers()
 
         elif k == 3:
+            self.random_new_numbers()
             self.random_new_numbers()
 
         self.update()
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Left:
-            self.kliked(-1)
+            self.clicked(-1)
         elif e.key() == Qt.Key_Right:
-            self.kliked(1)
+            self.clicked(1)
         elif e.key() == Qt.Key_Down:
-            self.kliked(0)
+            self.clicked(0)
         elif e.key() == Qt.Key_Up:
-            self.kliked(2)
+            self.clicked(2)
         elif e.key() == Qt.Key_Plus:
-            self.kliked(3)
-
+            self.clicked(3)
 
     def paintEvent(self, event):
         qp = QPainter()
         qp.setRenderHint(QPainter.Antialiasing)
 
         qp.begin(self)
-        pen = QPen(QColor('#34486a'))
+        pen = QPen(QColor(colors[0]))
         qp.setPen(pen)
         qp.setFont(QFont('Decorative', 30))
         for ex in range(4):
             for ey in range(4):
-                qp.fillRect(ex*x*1.2+x, ey*x*1.2+x, x, x, QBrush(QColor(color[int(map[ex][ey]/2)])))  # несовершенно
-                qp.drawText(ex*x*1.2+x, ey*x*1.2+x, x, x, Qt.AlignHCenter | Qt.AlignVCenter, str(map[ex][ey]))
+                qp.fillRect(ex * x * 1.2 + x, ey * x * 1.2 + x, x, x, QBrush(QColor(colors[len(str(bin(_map[ex][ey]))) - 2])))
+                qp.drawText(ex * x * 1.2 + x, ey * x * 1.2 + x, x, x, Qt.AlignHCenter | Qt.AlignVCenter, str(_map[ex][ey]))
         qp.end()
 
-
-    def random_new_numbers(self):
+    @staticmethod
+    def random_new_numbers():
         what_times = 0
         for i in range(50):
             eex = random.randrange(0, 4)
             eey = random.randrange(0, 4)
-            if map[eex][eey] == 0:
-                map[eex][eey] = random.choice((2, 2, 4))
+            if _map[eex][eey] == 0:
+                _map[eex][eey] = random.choice((0b_10, 0b_10, 0b_10, 0b_10, 0b_100))  # 10% fortune for 4
                 what_times += 1
             if what_times == 1:
                 break
 
 
-color = ['#f6f4f5', '#d7ddea', '#cad877']
+colors = ['#34486a', '#f6f4f5',
+          '#cad877', '#d7ddea', '#FEC77F', '#FFB3B1',
+          '#c0eb7e', '#c2e4eb', '#ffea97', '#f9c0c9',
+          '#778046', '#757980', '#c78b89']
 x = 53
-map = [[0 for i in range(4)] for ii in range(4)]
+_map = [[0 for i in range(4)] for ii in range(4)]  # частная переменная map
 
 
 if __name__ == '__main__':
