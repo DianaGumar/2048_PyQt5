47233621c5c11d73ff26f63b91001ff85109119f
GumarDiana <lantan.mp4@gmail.com>
2019-05-13 14:20:29 +0300

    add start/end and changed size map

--- main_my.py
+++ main_my.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import numpy as np
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -9,12 +10,15 @@ class Program(QWidget):
     def __init__(self, parent=None):
         super(Program, self).__init__(parent)
         self.setStyleSheet("background-color: white")
+
         self.Game_over = 0
         self.init_ui()
 
     def init_ui(self):
-        self.setGeometry(800, 150, 350, 500)
-        self.setWindowOpacity(0.9)
+        # self.setGeometry(800, 150, 350, 500)
+        self.setFixedSize(350, 500)
+        self.setWindowTitle("PyQt5_2048")
+        # self.setWindowOpacity(0.9)
         self.show()
 
     def set_map(self, mx, my, value):
@@ -28,7 +32,7 @@ class Program(QWidget):
 
     @staticmethod
     def exist_map(mx, my):
-        return 0 <= mx < 4 and 0 <= my < 4
+        return 0 <= mx < _size and 0 <= my < _size
 
     def turn(self, ex, ey, sx, sy):  # движение на одну клетку
 
@@ -58,12 +62,12 @@ class Program(QWidget):
     global moved
 
     def game_over(self):
-        for ex in range(4):
-            for ey in range(4):
+        for ex in range(_size):
+            for ey in range(_size):
                 if self.get_map(ex, ey) == 0:
                     return 0
-        for ex in range(4):
-            for ey in range(4):
+        for ex in range(_size):
+            for ey in range(_size):
                 if self.get_map(ex, ey) == self.get_map(ex + 1, ey) or self.get_map(ex, ey) == self.get_map(ex, ey + 1):
                     return 0
         return 1
@@ -73,10 +77,10 @@ class Program(QWidget):
         if k == -1:
             sys.stdout.write('\rleft')
             self.moved = 0
-            for ey in range(4):
-                for ex in range(-1, 4):
+            for ey in range(_size):
+                for ex in range(-1, _size):
                     self.turn(ex, ey, -1, 0)
-                for ex in range(-1, 4):
+                for ex in range(-1, _size):
                     self.join(ex, ey, -1, 0)
             if self.moved == 1:
                 self.random_new_numbers()
@@ -84,10 +88,10 @@ class Program(QWidget):
         elif k == 2:
             sys.stdout.write('\rup')
             self.moved = 0
-            for ex in range(4):
-                for ey in range(-1, 4):
+            for ex in range(_size):
+                for ey in range(-1, _size):
                     self.turn(ex, ey, 0, -1)
-                for ey in range(-1, 4):
+                for ey in range(-1, _size):
                     self.join(ex, ey, 0, -1)
             if self.moved == 1:
                 self.random_new_numbers()
@@ -96,7 +100,7 @@ class Program(QWidget):
             sys.stdout.write('\rright')
             self.moved = 0
             # sys.stdout.write(str(bin(map[1][1])))
-            for ey in range(4):
+            for ey in range(_size):
                 for ex in range(2, -1, -1):
                     self.turn(ex, ey, 1, 0)
                 for ex in range(2, -1, -1):
@@ -107,7 +111,7 @@ class Program(QWidget):
         elif k == 0:
             sys.stdout.write('\rdown')
             self.moved = 0
-            for ex in range(4):
+            for ex in range(_size):
                 for ey in range(2, -1, -1):
                     self.turn(ex, ey, 0, 1)
                 for ey in range(2, -1, -1):
@@ -116,8 +120,14 @@ class Program(QWidget):
                 self.random_new_numbers()
 
         elif k == 3:
-            self.random_new_numbers()
-            self.random_new_numbers()
+            if self.game_over() == 1:
+                for ex in range(_size):
+                    for ey in range(_size):
+                        self.set_map(ex, ey, 0)
+                _bill[1] = 0
+            if np.sum(_map) == 0:
+                self.random_new_numbers()
+                self.random_new_numbers()
 
         self.update()
 
@@ -140,8 +150,8 @@ class Program(QWidget):
         qp.begin(self)
         pen = QPen(QColor(colors[0]))
         qp.setPen(pen)
-        for ex in range(4):
-            for ey in range(4):
+        for ex in range(_size):
+            for ey in range(_size):
                 qp.setFont(QFont('Decorative', self.font_size(len(str(_map[ex][ey])))))  # ~30
                 if self.game_over() == 0:
                     qp.fillRect(ex * x * 1.2 + x, ey * x * 1.2 + x, x, x,
@@ -152,9 +162,9 @@ class Program(QWidget):
                             Qt.AlignHCenter | Qt.AlignVCenter, str(_map[ex][ey]))
 
         qp.setFont(QFont('Decorative', self.font_size(len(str(_bill[1])))))
-        qp.drawText(x * 2.3, x * 6, x * 2, x, Qt.AlignHCenter | Qt.AlignVCenter, str(_bill[1]))
+        qp.drawText(121.9, 318, 106, 53, Qt.AlignHCenter | Qt.AlignVCenter, str(_bill[1]))
         if self.game_over() == 1:
-            qp.drawText(x * 1.3, x * 7, x * 4, x, Qt.AlignHCenter | Qt.AlignVCenter, "Game Over")
+            qp.drawText(121.9, 318, 106, 53, Qt.AlignHCenter | Qt.AlignVCenter, "Game Over")
             qp.fillRect(x, x, x*4.6, x*4.6, QBrush(QColor(0, 0, 0, 50)))
         qp.end()
 
@@ -175,8 +185,8 @@ class Program(QWidget):
     def random_new_numbers():
         what_times = 0
         for i in range(50):
-            eex = random.randrange(0, 4)
-            eey = random.randrange(0, 4)
+            eex = random.randrange(0, _size)
+            eey = random.randrange(0, _size)
             if _map[eex][eey] == 0:
                 _map[eex][eey] = random.choice((0b_10, 0b_10, 0b_10, 0b_10, 0b_100))  # 10% fortune for 4
                 what_times += 1
@@ -188,9 +198,10 @@ colors = ['#34486a', '#f6f4f5',
           '#cad877', '#d7ddea', '#FEC77F', '#FFB3B1',
           '#c0eb7e', '#c2e4eb', '#ffea97', '#f9c0c9',
           '#778046', '#757980', '#c78b89']
-x = 53
-_map = [[0 for i in range(4)] for ii in range(4)]  # частная переменная map
-_bill = [0, 0]
+_size = 5
+x = _size * 9  # размер сетки и отступы 13 9
+_map = [[0 for i in range(_size)] for ii in range(_size)]  # частная переменная map
+_bill = [0, 0]  # счёт рекордный и настоящий
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
